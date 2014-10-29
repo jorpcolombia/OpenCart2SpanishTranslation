aeb05bc34ace00fbeba6711ea777f090002ec11e
jorpcolombia <jorpcolombia@gmail.com>
2014-10-29 11:14:51 -0600

    Traducir forgotten.php

--- catalog/language/spanish/account/forgotten.php
+++ catalog/language/spanish/account/forgotten.php
@@ -1,16 +1,16 @@
 <?php
 // Heading
-$_['heading_title']   = 'Forgot Your Password?';
+$_['heading_title']   = '¿Olvidó su contraseña?';
 
 // Text
-$_['text_account']    = 'Account';
-$_['text_forgotten']  = 'Forgotten Password';
-$_['text_your_email'] = 'Your E-Mail Address';
-$_['text_email']      = 'Enter the e-mail address associated with your account. Click submit to have your password e-mailed to you.';
-$_['text_success']    = 'Success: A new password has been sent to your e-mail address.';
+$_['text_account']    = 'Cuenta';
+$_['text_forgotten']  = 'Contraseña Olvidada';
+$_['text_your_email'] = 'Su E-Mail';
+$_['text_email']      = 'Escriba el e-mail asociado a su cuenta. Presiona enviar para recibir una nueva contraseña en su e-mail.';
+$_['text_success']    = 'Una nueva contraseña se envió a su e-mail.';
 
 // Entry
-$_['entry_email']     = 'E-Mail Address';
+$_['entry_email']     = 'E-Mail';
 
 // Error
-$_['error_email']     = 'Warning: The E-Mail Address was not found in our records, please try again!';
+$_['error_email']     = 'Advertencia: El E-Mail no se encontró en nuestros registros. Por favor intente de nuevo.';
