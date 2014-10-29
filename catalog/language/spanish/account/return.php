86be0808812dab7391cbf95854b25850f11f2b1e
jorpcolombia <jorpcolombia@gmail.com>
2014-10-29 14:01:37 -0600

    Traducir return.php

--- catalog/language/spanish/account/return.php
+++ catalog/language/spanish/account/return.php
@@ -1,39 +1,39 @@
 <?php
 // Heading
-$_['heading_title']      = 'Product Returns';
+$_['heading_title']      = 'Devoluciones';
 
 // Text
-$_['text_account']       = 'Account';
-$_['text_return']        = 'Return Information';
-$_['text_return_detail'] = 'Return Details';
-$_['text_description']   = 'Please complete the form below to request an RMA number.';
-$_['text_order']         = 'Order Information';
-$_['text_product']       = 'Product Information &amp; Reason for Return';
-$_['text_message']       = '<p>Thank you for submitting your return request. Your request has been sent to the relevant department for processing.</p><p> You will be notified via e-mail as to the status of your request.</p>';
-$_['text_return_id']     = 'Return ID:';
-$_['text_order_id']      = 'Order ID:';
-$_['text_date_ordered']  = 'Order Date:';
-$_['text_status']        = 'Status:';
-$_['text_date_added']    = 'Date Added:';
-$_['text_comment']       = 'Return Comments';
-$_['text_history']       = 'Return History';
-$_['text_empty']         = 'You have not made any previous returns!';
-$_['text_agree']         = 'I have read and agree to the <a href="%s" class="agree"><b>%s</b></a>';
+$_['text_account']       = 'Cuenta';
+$_['text_return']        = 'Información de devolución';
+$_['text_return_detail'] = 'Detalles de devolución';
+$_['text_description']   = 'Por favor complete el formulario a continuación para recibir un número de devolución.';
+$_['text_order']         = 'Información de Orden';
+$_['text_product']       = 'Información de producto y motivos de devolución';
+$_['text_message']       = '<p>Gracias por enviar la solicitud de devolución.</p><p>Será notificado por E-mail del estado de su solicitud.</p>';
+$_['text_return_id']     = 'ID de Devolución:';
+$_['text_order_id']      = 'ID de Orden:';
+$_['text_date_ordered']  = 'Fecha de la Orden:';
+$_['text_status']        = 'Estado:';
+$_['text_date_added']    = 'Fecha:';
+$_['text_comment']       = 'Comentarios de devolución';
+$_['text_history']       = 'Historia de devolución';
+$_['text_empty']         = 'No ha solicitado devoluciones!';
+$_['text_agree']         = 'He leído y acepto los <a href="%s" class="agree"><b>%s</b></a>';
 
 // Column
-$_['column_return_id']   = 'Return ID';
-$_['column_order_id']    = 'Order ID';
-$_['column_status']      = 'Status';
-$_['column_date_added']  = 'Date Added';
-$_['column_customer']    = 'Customer';
-$_['column_product']     = 'Product Name';
-$_['column_model']       = 'Model';
-$_['column_quantity']    = 'Quantity';
-$_['column_price']       = 'Price';
-$_['column_opened']      = 'Opened';
-$_['column_comment']     = 'Comment';
-$_['column_reason']      = 'Reason';
-$_['column_action']      = 'Action';
+$_['column_return_id']   = 'ID de Devolución';
+$_['column_order_id']    = 'ID de Orden';
+$_['column_status']      = 'Estado';
+$_['column_date_added']  = 'Fecha';
+$_['column_customer']    = 'Cliente';
+$_['column_product']     = 'Producto';
+$_['column_model']       = 'Modelo';
+$_['column_quantity']    = 'Cantidad';
+$_['column_price']       = 'Precio';
+$_['column_opened']      = 'Abierto';
+$_['column_comment']     = 'Comentario';
+$_['column_reason']      = 'Motivo';
+$_['column_action']      = 'Acción';
 
 // Entry
 $_['entry_order_id']     = 'Order ID';
@@ -41,24 +41,24 @@ $_['entry_date_ordered'] = 'Order Date';
 $_['entry_firstname']    = 'First Name';
 $_['entry_lastname']     = 'Last Name';
 $_['entry_email']        = 'E-Mail';
-$_['entry_telephone']    = 'Telephone';
-$_['entry_product']      = 'Product Name';
-$_['entry_model']        = 'Product Code';
-$_['entry_quantity']     = 'Quantity';
-$_['entry_reason']       = 'Reason for Return';
-$_['entry_opened']       = 'Product is opened';
-$_['entry_fault_detail'] = 'Faulty or other details';
-$_['entry_captcha']      = 'Enter the code in the box below';
+$_['entry_telephone']    = 'Teléfono';
+$_['entry_product']      = 'Producto';
+$_['entry_model']        = 'Código de Producto';
+$_['entry_quantity']     = 'Cantidad';
+$_['entry_reason']       = 'Motivo de Devolución';
+$_['entry_opened']       = 'El Producto llegó abierto';
+$_['entry_fault_detail'] = 'Defectuoso y otros detalles';
+$_['entry_captcha']      = 'Escriba el código en el campo a continuación';
 
 // Error
-$_['text_error']         = 'The returns you requested could not be found!';
-$_['error_order_id']     = 'Order ID required!';
-$_['error_firstname']    = 'First Name must be between 1 and 32 characters!';
-$_['error_lastname']     = 'Last Name must be between 1 and 32 characters!';
-$_['error_email']        = 'E-Mail Address does not appear to be valid!';
-$_['error_telephone']    = 'Telephone must be between 3 and 32 characters!';
-$_['error_product']      = 'Product Name must be greater than 3 and less than 255 characters!';
-$_['error_model']        = 'Product Model must be greater than 3 and less than 64 characters!';
-$_['error_reason']       = 'You must select a return product reason!';
-$_['error_captcha']      = 'Verification code does not match the image!';
-$_['error_agree']        = 'Warning: You must agree to the %s!';
+$_['text_error']         = 'No se encontró la devolución!';
+$_['error_order_id']     = 'ID de Orden requerido!';
+$_['error_firstname']    = 'El Nombre debe tener entre 1 and 32 caracteres!';
+$_['error_lastname']     = 'El Apellido debe tener entre 1 and 32 caracteres!';
+$_['error_email']        = 'El E-Mail no parece ser válido!';
+$_['error_telephone']    = 'El Teléfono debe tener entre 3 and 32 caracteres!';
+$_['error_product']      = 'El Producto debe tener entre 3 and less than 255 characters!';
+$_['error_model']        = 'El modelo de Producto debe tener entre 3 and less than 64 characters!';
+$_['error_reason']       = 'Debe Seleccionr un motivo de devolución!';
+$_['error_captcha']      = 'El código de verificación no coincide con la imagen!';
+$_['error_agree']        = 'Advertencia: Debe aceptar la %s!';
