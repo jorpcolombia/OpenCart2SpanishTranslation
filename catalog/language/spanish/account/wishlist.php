e0c1a84cde3fa2edbe50ce132df891b9f1c68338
jorpcolombia <jorpcolombia@gmail.com>
2014-10-29 14:57:56 -0600

    Traducir wishlist.php

--- catalog/language/spanish/account/wishlist.php
+++ catalog/language/spanish/account/wishlist.php
@@ -1,21 +1,21 @@
 <?php
 // Heading
-$_['heading_title'] = 'My Wish List';
+$_['heading_title'] = 'Mi Lista de Deseos';
 
 // Text
-$_['text_account']  = 'Account';
-$_['text_instock']  = 'In Stock';
-$_['text_wishlist'] = 'Wish List (%s)';
-$_['text_login']    = 'You must <a href="%s">login</a> or <a href="%s">create an account</a> to save <a href="%s">%s</a> to your <a href="%s">wish list</a>!';
-$_['text_success']  = 'Success: You have added <a href="%s">%s</a> to your <a href="%s">wish list</a>!';
-$_['text_exists']   = '<a href="%s">%s</a> is already in your <a href="%s">wish list</a>!';
-$_['text_remove']   = 'Success: You have modified your wish list!';
-$_['text_empty']    = 'Your wish list is empty.';
+$_['text_account']  = 'Cuenta';
+$_['text_instock']  = 'Disponible';
+$_['text_wishlist'] = 'Lista de deseos (%s)';
+$_['text_login']    = 'Debe <a href="%s">iniciar sesión</a> o <a href="%s">registrarse/a> para guardar <a href="%s">%s</a> en su <a href="%s">lista de deseos</a>';
+$_['text_success']  = 'Se ha agregado <a href="%s">%s</a> a la  <a href="%s">lista de deseos</a>!';
+$_['text_exists']   = '<a href="%s">%s</a> ya estaba en la <a href="%s">lista de deseos</a>!';
+$_['text_remove']   = 'Ha modificado existosamente la lista de deseos.';
+$_['text_empty']    = 'La lista de deseos está vacía.';
 
 // Column
-$_['column_image']  = 'Image';
-$_['column_name']   = 'Product Name';
-$_['column_model']  = 'Model';
-$_['column_stock']  = 'Stock';
-$_['column_price']  = 'Unit Price';
-$_['column_action'] = 'Action';
+$_['column_image']  = 'Imagen';
+$_['column_name']   = 'Producto';
+$_['column_model']  = 'Modelo';
+$_['column_stock']  = 'Inventario';
+$_['column_price']  = 'Precio Unitrio';
+$_['column_action'] = 'Acción';
