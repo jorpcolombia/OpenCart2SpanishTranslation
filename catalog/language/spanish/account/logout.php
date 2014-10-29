20b45ba36ab228054b713a8732a27cdf8e4c8824
jorpcolombia <jorpcolombia@gmail.com>
2014-10-29 11:21:06 -0600

    Traducir logout.php

--- catalog/language/spanish/account/logout.php
+++ catalog/language/spanish/account/logout.php
@@ -1,8 +1,8 @@
 <?php
 // Heading
-$_['heading_title'] = 'Account Logout';
+$_['heading_title'] = 'Cerrar Sesión';
 
 // Text
-$_['text_message']  = '<p>You have been logged off your account. It is now safe to leave the computer.</p><p>Your shopping cart has been saved, the items inside it will be restored whenever you log back into your account.</p>';
-$_['text_account']  = 'Account';
-$_['text_logout']   = 'Logout';
+$_['text_message']  = '<p>Ha cerrado su cuenta. Ahora es seguro dejar el computador.</p><p>El carrito de compras se ha guardado y se mostrará una vez inicie sesión nuevamente.</p>';
+$_['text_account']  = 'Cuenta';
+$_['text_logout']   = 'Cerrar Sesión';
