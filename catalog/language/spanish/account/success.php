bce279c3caf93339a62310d71c7b75313a1d9a39
jorpcolombia <jorpcolombia@gmail.com>
2014-10-29 14:38:09 -0600

    Traducir success.php

--- catalog/language/spanish/account/success.php
+++ catalog/language/spanish/account/success.php
@@ -1,9 +1,9 @@
 <?php
 // Heading
-$_['heading_title'] = 'Your Account Has Been Created!';
+$_['heading_title'] = 'Su cuenta ha sido creada!';
 
 // Text
-$_['text_message']  = '<p>Congratulations! Your new account has been successfully created!</p> <p>You can now a member of %s affiliates.</p> <p>If you have ANY questions about the operation of this affiliate system, please e-mail the store owner.</p> <p>A confirmation has been sent to the provided e-mail address. If you have not received it within the hour, please <a href="%s">contact us</a>.</p>';
-$_['text_approval'] = '<p>Thank you for registering with %s!</p><p>You will be notified by e-mail once your account has been activated by the store owner.</p><p>If you have ANY questions about the operation of this online shop, please <a href="%s">contact the store owner</a>.</p>';
-$_['text_account']  = 'Account';
-$_['text_success']  = 'Success';
+$_['text_message']  = '<p>¡Felicitaciones! Su cuenta ha sido creada.</p> <p>Ahora hace parte de %s miembros.</p> <p>Si tiene cualquier duda puede preguntar en el <a href="%s">Formulario de contacto</a>.</p>';
+$_['text_approval'] = '<p>¡Gracias por registrarse con %s!</p><p>Será notificado por E-mail cuando su cuenta sea activada por el administrador.</p><p>Si tiene cualquier duda puede preguntar en el <a href="%s">Formulario de contacto</a>.</p>';
+$_['text_account']  = 'Cuenta';
+$_['text_success']  = 'Éxito';
