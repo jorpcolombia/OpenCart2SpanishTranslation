852e3e7892280a290547611ac0c1eb1fc5615e85
jorpcolombia <jorpcolombia@gmail.com>
2014-10-29 11:22:26 -0600

    Traducir newsletter.php

--- catalog/language/spanish/account/newsletter.php
+++ catalog/language/spanish/account/newsletter.php
@@ -1,11 +1,11 @@
 <?php
 // Heading
-$_['heading_title']    = 'Newsletter Subscription';
+$_['heading_title']    = 'Boletín de Noticias';
 
 // Text
-$_['text_account']     = 'Account';
-$_['text_newsletter']  = 'Newsletter';
-$_['text_success']     = 'Success: Your newsletter subscription has been successfully updated!';
+$_['text_account']     = 'Cuenta';
+$_['text_newsletter']  = 'Boletín';
+$_['text_success']     = 'Su suscripción al boletín se ha actualizado exitosamente.';
 
 // Entry
-$_['entry_newsletter'] = 'Subscribe';
+$_['entry_newsletter'] = 'Suscribirse';
